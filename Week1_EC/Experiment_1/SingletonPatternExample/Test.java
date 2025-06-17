d7665bc498b0ea1c61933e4576f50218936da6e7
RamaTeja99 <2200032668@kluniversity.in>
2025-06-17 20:27:37 +0530

    Added Week1 Experiment1 Solution

---
+++ Week1_EC/Experiment_1/SingletonPatternExample/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+  public static void main(String[] args) {
+    Logger l1 = Logger.getInstance();
+    Logger l2 = Logger.getInstance();
+    Logger l3 = Logger.getInstance();
+    System.out.println(l1 == l2 && l2 == l3); //It shows that all the instances are same object by using singleton pattern
+    //If true it means same objects are created using singleton pattern else if false it means different objects are created
+  }
+}
